766d79e81e90de78c452ff8f1a16dd12f5a8a58a
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-02-26 21:56:09 -0600

    Adding stepMotor code

--- StepMotor_Interruptions/Sources/main.c
+++ StepMotor_Interruptions/Sources/main.c
@@ -1,67 +1,112 @@
 #include "derivative.h" 
 
-unsigned char tmp_ADC=0;
+void LPTM_init(void);
+void RGB_init(void);
+void shiftLEDs(void);
+void adcInit(void);
+void portsInit(void);
+void shift(void);
 
-void delay(unsigned long tiempo_ms) {
-	LPTMR0_PSR = 0b00000101;
-	LPTMR0_CMR = tiempo_ms;
-	LPTMR0_CSR = 0b01000001;  //1; //0x01 // 0x00000001
-	while (!(LPTMR0_CSR&0b10000000));
-	LPTMR0_CSR = 0;
-}
+unsigned int temp;
+unsigned long LEDs[3];
+unsigned long tmp0;
+unsigned char timerStateReached;
+unsigned char i, j;
+unsigned long secuencia[8] = { 0x00000008, 0x0000000C, 0x00000004, 0x00000006,
+		0x00000002, 0x00000003, 0x00000001, 0x00000009 };
+unsigned long secuenciaR[8] = { 0x00000009, 0x00000001, 0x00000003, 0x00000002,
+		0x00000006, 0x00000004, 0x0000000C, 0x00000008 };
 
 int main(void) {
-	unsigned long secuencia[8] = { 0x00000008, 0x0000000C, 0x00000004, 0x00000006, 0x00000002, 0x00000003, 0x00000001, 0x00000009 };
-	unsigned long secuenciaR[8] = { 0x00000009, 0x00000001, 0x00000003, 0x00000002, 0x00000006, 0x00000004, 0x0000000C, 0x00000008 };
-	unsigned char i = 0, j = 0;
+	LEDs[0] = 0;
+	LEDs[1] = 1;
+	LEDs[2] = 1;
+	timerStateReached = 0;
+	j = i = 0;
 
-	SIM_SCGC5 = 0x00001C01;
-	SIM_SCGC6 = 0x08000001;
+	RGB_init();
+	LPTM_init();
+	adcInit();
 
-	ADC0_CFG2 = 1 << 4;
-	ADC0_SC1A =(1<<6);		//Enable ADC interrupt and channel select
+	return 0;
+}
 
+void portsInit(void) {
+	SIM_SCGC5 |= (1 << 10);
 	PORTB_PCR0 =0x00000100; //PTB0 GPIO
 	PORTB_PCR1 =0x00000100; //PTB1 GPIO
 	PORTB_PCR2 =0x00000100; //PTB2 GPIO
 	PORTB_PCR3 =0x00000100; //PTB3 GPIO
 	PORTD_PCR4 =0x00000100; //PTB5 GPIO
 	PORTC_PCR2 =0x00000000; //PTC2 GPIO
-
-	NVIC_ICPR = (1 << 28) + (1 << 15);		//Disable flags
-	NVIC_ISER = (1 << 28) + (1 << 15);		//Enable LPTMR0 & ADC interruptions
-
-//	LPTMR0_PSR=(1<<2)+1; 	//Bypass prescaler, 1KHz base de tiempo
-//  LPTMR0_CMR=10;			//Start to convert every 10ms
-//  LPTMR0_CSR=(1<<6)+1; 	//Enable Interrupt, enable
-
 	GPIOB_PDDR = 0x0000000F;
 	GPIOD_PDDR = 0x00000000;
+}
 
-	while(1){
+void adcInit(void) {
+	SIM_SCGC6 |= (1 << 27);
+	SIM_SCGC5 |= (1 << 13);
+	NVIC_ICPR |= (1 << 15);
+	NVIC_ISER |= (1 << 15);
+	//ADC0
+	ADC0_CFG1 = 0x00000000;
+	ADC0_CFG2 = (1 << 4);		//Seleccionar el canal B del ADC
+	ADC0_SC1A =0b1000100;//Habilitar interrupciones del ADC y el canal AD4-> esta en el canal PTE21	
+}
 
-		if((GPIOD_PDIR&0x00000010)==0x00000010){
-			GPIOB_PDOR = secuencia[i++];
-			delay(tmp_ADC);
-			if (i == 8) i = 0;
-		} 
-		else{
-			GPIOB_PDOR = secuenciaR[j++];
-			delay(tmp_ADC);
-			if (j == 8)  j = 0;
-		}
-		ADC0_SC1A=0x10000;
-		while (ADC0_SC2&0x80);
-	}
-	return 0;
+void LPTM_init(void) {
+	SIM_SCGC5 |= (1 << 0); //Activate the LPTMR in the system control gating register
+	LPTMR0_PSR = 0b00000101; //Bypass the preescaler and select the LPO(low power oscilator of 1Khz as the source of the timer)
+	LPTMR0_CMR = 500;			//compare of 500 clock cycles = .5 secs
+	LPTMR0_CSR = 0b01000001;	//Activate the timer and enable interrupts	
+	NVIC_ICPR |= (1 << 28);		//Clean flag of LPTM in the interrupt vector
+	NVIC_ISER |= (1 << 28);		//Activate the LPTM interrupt
+}
+
+void RGB_init(void) {
+	//First activate SIM_SCGC5 (system control gating register 5) for port B and D
+	SIM_SCGC5 |= (1 << 10);	//Port B
+	SIM_SCGC5 |= (1 << 12);	//Port D
+	PORTB_PCR18 = (1<<8);	//Set PTB18 as GPIO
+	PORTB_PCR19 = (1<<8);	//set PTB19 as GPIO
+	PORTD_PCR1 = (1<<8);	//Set PTD1 as GPIO
+	GPIOB_PDDR = (1 << 18);	//Set PTB18 as output
+	GPIOB_PDDR |= (1 << 19);	//set PTB19 as output
+	GPIOD_PDDR = (1 << 1);	//Set PTD1 as output
 }
 
 void ADC0_IRQHandler() {
-	tmp_ADC = ADC0_RA ;
-	
+	if ((ADC0_SC1A &(1<<7))==(1<<7)) {
+		temp = ((ADC0_RA/255)*1000)+325;
+		if(timerStateReached==1) {
+			LPTMR0_CMR = temp;
+			timerStateReached=0;
+		}
+		ADC0_SC1A |=4;
+	}
 }
 
 void LPTimer_IRQHandler() {
-	LPTMR0_CSR |= (1 << 7); 	//Clear flag
-	ADC0_SC1A |=4;
+	LPTMR0_CSR |= (1 << 7);	//Clear timer compare flag
+	timerStateReached = 1;
+	shiftLEDs();
+	shift();
 }
+
+void shift(void) {
+	GPIOB_PDOR |= (secuencia[++i]&0x00000001);
+	if (i == 8) {
+		i = 0;
+	}
+}
+
+void shiftLEDs(void) {
+	GPIOB_PDOR = (LEDs[0] << 18);
+	GPIOB_PDOR |= (LEDs[1] << 19);
+	GPIOD_PDOR = (LEDs[2] << 1);
+	tmp0 = LEDs[0];
+	LEDs[0] = LEDs[1];
+	LEDs[1] = LEDs[2];
+	LEDs[2] = tmp0;
+}
+
