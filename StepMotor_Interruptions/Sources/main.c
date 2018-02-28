e9cd29e799c0d7dfb9ecea6f7a66fc02939678c2
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-02-28 15:18:56 -0600

    Removing variables

--- StepMotor_Interruptions/Sources/main.c
+++ StepMotor_Interruptions/Sources/main.c
@@ -11,24 +11,20 @@ unsigned int temp;
 unsigned long LEDs[3];
 unsigned long tmp0;
 unsigned char timerStateReached;
-unsigned char i, j;
+unsigned char i;
 unsigned long secuencia[8] = { 0x00000008, 0x0000000C, 0x00000004, 0x00000006,
 		0x00000002, 0x00000003, 0x00000001, 0x00000009 };
-unsigned long secuenciaR[8] = { 0x00000009, 0x00000001, 0x00000003, 0x00000002,
-		0x00000006, 0x00000004, 0x0000000C, 0x00000008 };
 
 int main(void) {
 	LEDs[0] = 0;
 	LEDs[1] = 1;
 	LEDs[2] = 1;
 	timerStateReached = 0;
-	j = i = 0;
-
+	i = 0;
 	RGB_init();
 	portsInit();
 	LPTM_init();
 	adcInit();
-
 	return 0;
 }
 
@@ -49,12 +45,12 @@ void adcInit(void) {
 	//ADC0
 	ADC0_CFG1 = 0x00000000;
 	ADC0_CFG2 = (1 << 4);		//Seleccionar el canal B del ADC
-	ADC0_SC1A =0b1000100;//Habilitar interrupciones del ADC y el canal AD4-> esta en el canal PTE21	
+	ADC0_SC1A =0b1000100;		//Habilitar interrupciones del ADC y el canal AD4-> esta en el canal PTE30	
 }
 
 void LPTM_init(void) {
-	SIM_SCGC5 |= (1 << 0); //Activate the LPTMR in the system control gating register
-	LPTMR0_PSR = 0b00000101; //Bypass the preescaler and select the LPO(low power oscilator of 1Khz as the source of the timer)
+	SIM_SCGC5 |= (1 << 0); 		//Activate the LPTMR in the system control gating register
+	LPTMR0_PSR = 0b00000101; 	//Bypass the preescaler and select the LPO(low power oscilator of 1Khz as the source of the timer)
 	LPTMR0_CMR = 500;			//compare of 500 clock cycles = .5 secs
 	LPTMR0_CSR = 0b01000001;	//Activate the timer and enable interrupts	
 	NVIC_ICPR |= (1 << 28);		//Clean flag of LPTM in the interrupt vector
@@ -62,15 +58,15 @@ void LPTM_init(void) {
 }
 
 void RGB_init(void) {
-	//First activate SIM_SCGC5 (system control gating register 5) for port B and D
-	SIM_SCGC5 |= (1 << 10);	//Port B
-	SIM_SCGC5 |= (1 << 12);	//Port D
-	PORTB_PCR18 = (1<<8);	//Set PTB18 as GPIO
-	PORTB_PCR19 = (1<<8);	//set PTB19 as GPIO
-	PORTD_PCR1 = (1<<8);	//Set PTD1 as GPIO
-	GPIOB_PDDR = (1 << 18);	//Set PTB18 as output
+	//Activate SIM_SCGC5 (system control gating register 5) for port B and D
+	SIM_SCGC5 |= (1 << 10);		//Port B
+	SIM_SCGC5 |= (1 << 12);		//Port D
+	PORTB_PCR18 = (1<<8);		//Set PTB18 as GPIO
+	PORTB_PCR19 = (1<<8);		//set PTB19 as GPIO
+	PORTD_PCR1 = (1<<8);		//Set PTD1 as GPIO
+	GPIOB_PDDR = (1 << 18);		//Set PTB18 as output
 	GPIOB_PDDR |= (1 << 19);	//set PTB19 as output
-	GPIOD_PDDR = (1 << 1);	//Set PTD1 as output
+	GPIOD_PDDR = (1 << 1);		//Set PTD1 as output
 }
 
 void ADC0_IRQHandler() {
