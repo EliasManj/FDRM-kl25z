5a06a737bdd044be8577e80d8f8f8909af98f942
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-03-09 14:54:25 -0600

    Adding pointers to timer mmodule

--- Timer_Module/Sources/main.c
+++ Timer_Module/Sources/main.c
@@ -6,22 +6,55 @@
 #include "derivative.h" /* include peripheral declarations */
 
 void timerInit(void);
+void RGB_init(void);
+unsigned int i = 0;
+
+volatile unsigned long *pointerPTE29;
 
 int main(void) {
-	int counter = 0;
 
-	for (;;) {
-		counter++;
-	}
+	RGB_init();
+	timerInit();
+	GPIOE_PDOR = (1<<1);
+	pointerPTE29 = &GPIOE_PDIR;
+	
+	*((volatile unsigned long *)0x400FF0C0) = *pointerPTE29;
+	GPIOB_PDOR = (1<<19);
+	GPIOB_PDOR |= (1<<18);
+	do {
+		do {
+		} while (!(TPM0_C2SC &(1<<7)));
+		TPM0_C2SC|=(1<<7);		//Apagar bandera
+		
+		//GPIOD_PDOR = (i<<1);
+		//i=~i&0x00000001;
+	} while(1);
 
 	return 0;
 }
 
-void timerInit(void){
-	SIM_SCGC6 |= (1<<24);		//CLK TMP0
-	SIM_SCGC5 |= (1<<13);		//PTE29?
+void timerInit(void) {
+	//Setiing MCGIRCLK
+	SIM_SCGC6 |= (1 << 24);		//CLK TMP0
+	SIM_SCGC5 |= (1 << 13);		//PTE29
 	PORTE_PCR29 = (3<<8);		//TMP0_CH2
-	MCG_C1 |= (1<<1);			//IRCLKEN
+	MCG_C1 |= (1 << 1);			//IRCLKEN
 	MCG_C2 |= 1;				//IRCS = 1 (Fast clock) 4MHz
-	MCG_SC = (2<<1);			//FCRDIV = 4, divide by 4 = 1MHz
+	MCG_SC = (2 << 1);			//FCRDIV = 4, divide by 4 = 1MHz
+	SIM_SOPT2 = (3 << 24);		//TPM clock source select TMPSRC
+	TPM0_SC = (1 << 3);			//CMOD select clock mode mux
+	TPM0_C2SC = (5<<2);			//Output compare -> toggle mode FOR TMP0 CH2
+	TPM0_C2V =0x0000FFFF;		//Maximun value for 16 bits 65535
+}
+
+void RGB_init(void) {
+	//Activate SIM_SCGC5 (system control gating register 5) for port B and D
+	SIM_SCGC5 |= (1 << 10);		//Port B
+	SIM_SCGC5 |= (1 << 12);		//Port D
+	PORTB_PCR18 = (1<<8);		//Set PTB18 as GPIO
+	PORTB_PCR19 = (1<<8);		//set PTB19 as GPIO
+	PORTD_PCR1 = (1<<8);		//Set PTD1 as GPIO
+	GPIOB_PDDR = (1 << 18);		//Set PTB18 as output
+	GPIOB_PDDR |= (1 << 19);	//set PTB19 as output
+	GPIOD_PDDR = (1 << 1);		//Set PTD1 as output
 }
