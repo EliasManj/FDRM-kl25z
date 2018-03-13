85116372444eec33b39d2b9bb1353619ebe33f74
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-03-12 21:38:11 -0600

    Adding UART INIT

--- Motor_UART_Controller/Sources/main.c
+++ Motor_UART_Controller/Sources/main.c
@@ -7,6 +7,7 @@ void ADC_init(void);
 void Ports_init(void);
 void shift(void);
 void timer_Init(void);
+void UART_init(void);
 
 unsigned int temp;
 unsigned long LEDs[3];
@@ -24,14 +25,15 @@ int main(void) {
 	i = 0;
 	RGB_init();
 	Ports_init();
-	//timer_Init();
-	LPTM_init();
+	timer_Init();
+	//LPTM_init();
 	ADC_init();
 	return 0;
 }
 
-void FTM0_IRQHandler(){
-	TPM0_SC |= (1<<7); //TOF clear interrupt flag
+void FTM0_IRQHandler() {
+	TPM0_SC |= (1 << 7); 		//TOF clear interrupt flag
+	TPM0_C2SC |= (1<<7);
 	timerStateReached = 1;
 	shift();
 	shiftLEDs();
@@ -41,7 +43,7 @@ void timer_Init(void) {
 	//Setiing MCGIRCLK
 	SIM_SCGC6 |= (1 << 24);		//CLK TMP0
 	SIM_SCGC5 |= (1 << 13);		//PTE29
-	PORTE_PCR29 = (3<<8);		//TMP0_CH2
+	PORTE_PCR29 |= (3<<8);		//TMP0_CH2
 	MCG_C1 |= (1 << 1);			//IRCLKEN
 	MCG_C2 |= 1;				//IRCS = 1 (Fast clock) 4MHz
 	MCG_SC = (2 << 1);			//FCRDIV = 4, divide by 4 = 1MHz
@@ -50,8 +52,8 @@ void timer_Init(void) {
 	TPM0_SC |= (1 << 8);		//DMA enable overflow
 	TPM0_C2SC = (5<<2);			//Output compare -> toggle mode FOR TMP0 CH2
 	TPM0_C2V =0x0000FFFF;		//Maximun value for 16 bits 65535
-    NVIC_ICPR |= (1 << 17);
-    NVIC_ISER |= (1 << 17);
+	NVIC_ICPR |= (1 << 17);
+	NVIC_ISER |= (1 << 17);
 	TPM0_SC |= (1 << 3);		//CMOD select clock mode mux
 }
 
@@ -72,12 +74,12 @@ void ADC_init(void) {
 	//ADC0
 	ADC0_CFG1 = 0x00000000;
 	ADC0_CFG2 = (0 << 4);		//Seleccionar el canal A del ADC
-	ADC0_SC1A =0b1000100;		//Habilitar interrupciones del ADC y el canal AD4-> esta en el canal PTE21
+	ADC0_SC1A =0b1000100;//Habilitar interrupciones del ADC y el canal AD4-> esta en el canal PTE21
 }
 
 void LPTM_init(void) {
-	SIM_SCGC5 |= (1 << 0); 		//Activate the LPTMR in the system control gating register
-	LPTMR0_PSR = 0b00000101; 	//Bypass the preescaler and select the LPO(low power oscilator of 1Khz as the source of the timer)
+	SIM_SCGC5 |= (1 << 0); //Activate the LPTMR in the system control gating register
+	LPTMR0_PSR = 0b00000101; //Bypass the preescaler and select the LPO(low power oscilator of 1Khz as the source of the timer)
 	LPTMR0_CMR = 500;			//compare of 500 clock cycles = .5 secs
 	NVIC_ICPR |= (1 << 28);		//Clean flag of LPTM in the interrupt vector
 	NVIC_ISER |= (1 << 28);		//Activate the LPTM interrupt
@@ -100,7 +102,7 @@ void ADC0_IRQHandler() {
 	if ((ADC0_SC1A &(1<<7))==(1<<7)) {
 		temp = ((ADC0_RA*1000)/225)+225;
 		if(timerStateReached==1) {
-			LPTMR0_CMR = temp;
+			//LPTMR0_CMR = temp;
 			timerStateReached=0;
 		}
 		ADC0_SC1A |=4;
@@ -129,4 +131,14 @@ void shiftLEDs(void) {
 	LEDs[0] = LEDs[1];
 	LEDs[1] = LEDs[2];
 	LEDs[2] = tmp0;
+}
+
+void UART_init(void) {//Se configura un canal de recepción y transmisión E_20 Tx y E_21 Rx
+	SIM_SCGC5 |= (1 << 13);		//RELOJ PORTE
+	SIM_SCGC4 |= (1 << 10); 	//RELOJ UART
+	PORTE_PCR20 =(1<<10);	//UART0 pin Tx (alt4)
+	PORTE_PCR21 =(1<<10);	//UART0 pin RX (alt4)
+	UART0_BDL = 137;		//Clk=640*32768, baud rate 9600bps porqué BDL=137??
+	UART0_C2 = 12;			//Habilitando TX y RX (TE y RE en el registro)
+
 }
