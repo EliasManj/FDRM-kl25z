ad8c8a152e7c9c36ff4a7f30adba564e9beb4752
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-03-30 21:06:26 -0600

    Importing uart code

--- Motor_UART_Controller/Sources/main.c
+++ Motor_UART_Controller/Sources/main.c
@@ -1,9 +1,23 @@
 #include "derivative.h" 
 
+//ASCII codes
+#define BUFLEN 		16
+#define NEW_LINE 	0x0A
+#define CARR_RETURN 0x0D
+#define BACKSPACE 	0x08
+
+//Commands encoding
+#define DIR_CW 		1
+#define DIR_CCW		2
+#define MOTOR_ON 	3
+#define MOTOR_OFF 	4
+#define STEP_CW		5
+#define TEMPLIMIT 	6
+
+//Low power Timer and TPM
 void LPTM_init(void);
 void RGB_init(void);
 void shiftLEDs(void);
-void ADC_init(void);
 void Ports_init(void);
 void shift(void);
 void Timer_init(void);
@@ -11,11 +25,61 @@ void UART_init(void);
 void Set_timer_signal_GPIO(void);
 void Toggle_signal(void);
 
+//ADC
+void ADC_init(void);
+
+//UART
+void uart_init(void);
+
+//Define Command struct
+struct Command {
+	volatile uint8_t size;
+	volatile uint8_t n_items;
+	volatile uint8_t is_full;
+	volatile char data[BUFLEN];
+};
+
+typedef struct Command CommandString;
+CommandString commandString = { BUFLEN, 0, 0, { } };
+CommandString *commandString_p;
+
+uint8_t string_compare(volatile char *array1, volatile char *array2);
+void strcopy(volatile char *dest, volatile char *source);
+void command_add_item(CommandString *commandString, char item);
+void command_clear(CommandString *commandString);
+uint8_t command_compare_cmd_ugly(CommandString *commandString);
+
+//Define Buffer
+struct Buffer {
+	volatile uint8_t head;
+	volatile uint8_t tail;
+	volatile uint8_t size;
+	volatile char data[BUFLEN];
+};
+
+typedef struct Buffer bufferType;
+bufferType Buffer_rx = { 0, 0, BUFLEN, { } };
+bufferType *rx_bf;
+
+void uart_init(void);
+void buffer_push(bufferType *bf, char data);
+char buffer_pop(bufferType *bf);
+uint8_t buffer_inc(uint8_t pointer, uint8_t size);
+uint8_t buffer_isempty(bufferType *bf);
+uint8_t buffer_len(bufferType *bf);
+uint8_t buffer_isfull(bufferType *bf);
+void uart_send_done(bufferType *bf);
+
+//Define variables
+int rx_status;
+char command[BUFLEN];
+char val;
+int cmd_code;
 unsigned int temp;
 unsigned long LEDs[3];
 unsigned long tmp0;
 unsigned char timerStateReached;
-unsigned char i;
+unsigned char motorSequenceIndex;
 unsigned long secuencia[8] = { 0x00000008, 0x0000000C, 0x00000004, 0x00000006,
 		0x00000002, 0x00000003, 0x00000001, 0x00000009 };
 
@@ -25,15 +89,19 @@ int main(void) {
 	LEDs[1] = 1;
 	LEDs[2] = 1;
 	timerStateReached = 0;
-	i = 0;
+	motorSequenceIndex = 0;
 	RGB_init();
 	Ports_init();
 	Timer_init();
 	LPTM_init();
 	ADC_init();
 	Set_timer_signal_GPIO();
-	//UART_init();
+	//uart_init();
 	return 0;
+}
+
+void UART0_IRQHandler(void) {
+	
 }
 
 void FTM0_IRQHandler() {
@@ -46,19 +114,37 @@ void TPM0_IRQHandler(void) {
 	TPM0_SC |= (1 << 7);
 }
 
+void ADC0_IRQHandler() {
+	if ((ADC0_SC1A &(1<<7))==(1<<7)) {
+		temp = ((ADC0_RA*1000)/225)+225;
+		if(timerStateReached==1) {
+			LPTMR0_CMR = temp;
+			timerStateReached=0;
+		}
+		ADC0_SC1A |=4;
+	}
+}
+
+void LPTimer_IRQHandler() {
+	LPTMR0_CSR |= (1 << 7);	//Clear timer compare flag
+	timerStateReached = 1;
+	shift();
+	shiftLEDs();
+}
+
 void Timer_init(void) {
 	//Setiing MCGIRCLK
 	SIM_SCGC6 |= (1 << 24);		//CLK TMP0
 	SIM_SCGC5 |= (1 << 13);		//PTE29
-	PORTE_PCR29 |= (3<<8);		//TMP0_CH2
+	PORTE_PCR29 |= (3<<8);		//TMP0_CH2 for PTE29
 	MCG_C1 |= (1 << 1);			//IRCLKEN
 	MCG_C2 |= 1;				//IRCS = 1 (Fast clock) 4MHz
 	MCG_SC = (2 << 1);			//FCRDIV = 4, divide by 4 = 1MHz
-	SIM_SOPT2 = (3 << 24);		//TPM clock source select TMPSRC
+	SIM_SOPT2 |= (3 << 24);		//TPM clock source select TMPSRC
 	TPM0_SC |= (1 << 6);		//TOIE enable interrupt
 	TPM0_SC |= (1 << 8);		//DMA enable overflow
 	TPM0_C2SC = (5<<2);			//Output compare -> toggle mode FOR TMP0 CH2
-	TPM0_C2V =0x0000FFFF;		//Maximun value for 16 bits 65535
+	TPM0_C2V =0x0000FFFF;		//Maximun value for 16 bits 65535 -> 65535 clock cycles of 1MHz
 	NVIC_ICPR |= (1 << 17);
 	NVIC_ISER |= (1 << 17);
 	TPM0_SC |= (1 << 3);		//CMOD select clock mode mux
@@ -105,28 +191,10 @@ void RGB_init(void) {
 	GPIOD_PDDR = (1 << 1);		//Set PTD1 as output
 }
 
-void ADC0_IRQHandler() {
-	if ((ADC0_SC1A &(1<<7))==(1<<7)) {
-		temp = ((ADC0_RA*1000)/225)+225;
-		if(timerStateReached==1) {
-			LPTMR0_CMR = temp;
-			timerStateReached=0;
-		}
-		ADC0_SC1A |=4;
-	}
-}
-
-void LPTimer_IRQHandler() {
-	LPTMR0_CSR |= (1 << 7);	//Clear timer compare flag
-	timerStateReached = 1;
-	shift();
-	shiftLEDs();
-}
-
 void shift(void) {
-	GPIOB_PDOR = ((secuencia[i++]) & 0x0000000F);
-	if (i == 8) {
-		i = 0;
+	GPIOB_PDOR = ~((secuencia[motorSequenceIndex++]) & 0x0000000F);
+	if (motorSequenceIndex == 8) {
+		motorSequenceIndex = 0;
 	}
 }
 
@@ -140,13 +208,18 @@ void shiftLEDs(void) {
 	LEDs[2] = tmp0;
 }
 
-void UART_init(void) {			//Se configura un canal de recepción y transmisión E_20 Tx y E_21 Rx
-	SIM_SCGC5 |= (1 << 13);		//RELOJ PORTE
-	SIM_SCGC4 |= (1 << 10); 	//RELOJ UART
-	PORTE_PCR20 =(1<<10);		//UART0 pin Tx (alt4)
-	PORTE_PCR21 =(1<<10);		//UART0 pin RX (alt4)
-	UART0_BDL = 137;			//Clk=640*32768, baud rate 9600bps porqué BDL=137??
-	UART0_C2 = 12;				//Habilitando TX y RX (TE y RE en el registro)
+void uart_init(void) {
+	SIM_SCGC4 |= (1 << 10);	//CLK UART0
+	SIM_SCGC5 |= (1 << 9);	//CLOCK for PORTA
+	SIM_SOPT2 |= (1 << 26);	//Enable UART0 clock with MCGFLLCLK clock or MCGPLLCLK/2 clock
+	PORTA_PCR1 = (1<<9);	//Port control for UART_0
+	PORTA_PCR2 = (1<<9);	//Port control for UART_0
+	UART0_BDL = 137;		//clock=640*32768, baud rate 9600
+	UART0_C2 |= (1 << 5);		//reciver interrupt enable for RDRF
+	UART0_C2 |= (1 << 2);		//RE reciver enable
+	UART0_C2 |= (1 << 3);		//TE Transmiter enable
+	NVIC_ISER |= (1 << 12);
+	NVIC_ICPR |= (1 << 12);
 }
 
 void Set_timer_signal_GPIO(void){
