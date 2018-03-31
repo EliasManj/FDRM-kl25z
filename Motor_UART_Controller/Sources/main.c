486b870ffb3060195fec6d4625fd53e71f5bab2e
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-03-30 21:07:53 -0600

    setting buffer type

--- Motor_UART_Controller/Sources/main.c
+++ Motor_UART_Controller/Sources/main.c
@@ -233,3 +233,49 @@ void Set_timer_signal_GPIO(void){
 void Toggle_signal(void){
 	GPIOC_PTOR = (1<<3);
 }
+
+void buffer_push(bufferType *bf, char data) {
+	bf->data[bf->tail] = data;
+	bf->tail = buffer_inc(bf->tail, bf->size);
+}
+
+char buffer_pop(bufferType *bf) {
+	uint8_t item = bf->data[bf->head];
+	bf->head = buffer_inc(bf->head, bf->size);
+	return item;
+}
+
+uint8_t buffer_inc(uint8_t pointer, uint8_t size) {
+	uint8_t i = pointer;
+	if (++i >= size) {
+		i = 0;
+	}
+	return i;
+}
+
+uint8_t buffer_isempty(bufferType *bf) {
+	return bf->head == bf->tail;
+}
+
+uint8_t buffer_isfull(bufferType *bf) {
+	return buffer_len(bf) == (bf->size - 1);
+}
+
+uint8_t buffer_len(bufferType *bf) {
+	uint8_t len = bf->tail - bf->head;
+	if (len < 0) {
+		len += bf->size;
+	}
+	return len;
+}
+
+void uart_send_done(bufferType *bf) {
+	buffer_push(bf, 'D');
+	buffer_push(bf, 'O');
+	buffer_push(bf, 'N');
+	buffer_push(bf, 'E');
+	buffer_push(bf, 0x0D);
+	buffer_push(bf, 0x0A);
+	UART0_C2 |= 0x80;	//Turn on TX interrupt
+}
+
